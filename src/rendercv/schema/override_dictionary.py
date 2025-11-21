ca8e6f41877df71ca5b7469f2efcf0ee4d8b3e79
Sina Atalay <79940989+sinaatalay@users.noreply.github.com>
2025-11-21 19:42:59 +0300

    Remove old test files

---
+++ src/rendercv/schema/override_dictionary.py
@@ -0,0 +1,80 @@
+import copy
+from typing import overload
+
+
+@overload
+def update_value_by_location(
+    dict_or_list: dict,
+    key: str,
+    value: str,
+    full_key: str,
+) -> dict: ...
+@overload
+def update_value_by_location(
+    dict_or_list: list,
+    key: str,
+    value: str,
+    full_key: str,
+) -> list: ...
+def update_value_by_location(
+    dict_or_list: dict | list,
+    key: str,
+    value: str,
+    full_key: str,
+) -> dict | list:
+    """Set or update a value in a dictionary for the given key. For example, a key can
+    be `cv.sections.education.3.institution` and the value can be "Bogazici University".
+
+    Args:
+        dictionary: The dictionary to set or update the value.
+        key: The key to set or update the value.
+        value: The value to set or update.
+        sub_dictionary: The sub dictionary to set or update the value. This is used for
+            recursive calls. Defaults to None.
+    """
+    keys = key.split(".")
+    last_key: str | int = keys[-1]
+    remaining_key = ".".join(keys[:-1])
+    previous_key = ".".join(full_key.split(".")[:-1])
+
+    if isinstance(dict_or_list, list):
+        try:
+            last_key = int(last_key)
+        except ValueError as e:
+            message = (
+                f"`{previous_key}` corresponds to a list, but `{key}` is not an"
+                " integer."
+            )
+            raise ValueError(message) from e
+
+        if last_key >= len(dict_or_list):
+            message = f"Index {last_key} is out of range for the list `{previous_key}`."
+            raise IndexError(message)
+    elif not isinstance(dict_or_list, dict):
+        message = f"It seems like there's something wrong with {full_key}."
+        raise ValueError(message)
+
+    if len(keys) == 1:
+        new_value = value
+    else:
+        new_value = update_value_by_location(
+            dict_or_list[last_key],  # pyright: ignore[reportArgumentType, reportCallIssue]
+            remaining_key,
+            value,
+            full_key=full_key,
+        )
+
+    dict_or_list[last_key] = new_value  # pyright: ignore[reportArgumentType, reportCallIssue]
+
+    return dict_or_list
+
+
+def apply_overrides_to_dictionary(
+    dictionary: dict,
+    overrides: dict[str, str],
+) -> dict:
+    new_dictionary = copy.deepcopy(dictionary)
+    for key, value in overrides.items():
+        new_dictionary = update_value_by_location(new_dictionary, key, value, key)
+
+    return new_dictionary
--- tests/old_tests/testdata/test_copy_theme_files_to_output_directory_custom_theme/dummytheme/__init__.py
+++
@@ -1,7 +0,0 @@
-from typing import Literal
-
-import pydantic
-
-
-class DummythemeTheme(pydantic.BaseModel):
-    theme: Literal["dummytheme"]
