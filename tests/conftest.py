0dac92f1571b4898e93d55caf189eaec3aa3d2a3
Sina Atalay <79940989+sinaatalay@users.noreply.github.com>
2025-11-21 13:44:46 +0300

    Fix design tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,22 @@ import pathlib
 import pytest
 
 
+def pytest_addoption(parser: pytest.Parser) -> None:
+    parser.addoption(
+        "--update-testdata",
+        action="store_true",
+        default=False,
+        help="Update the updatable testdata",
+    )
+
+
 @pytest.fixture
-def testdata_dir(request):
+def update_testdata(request: pytest.FixtureRequest) -> bool:
+    return request.config.getoption("--update-testdata")
+
+
+@pytest.fixture
+def testdata_dir(request: pytest.FixtureRequest) -> pathlib.Path:
     module_path = pathlib.Path(request.node.module.__file__)
     module_name = module_path.stem
     base_dir = module_path.parent
