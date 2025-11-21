f16aca9dbf2b5da5af12aa4c40db7d3656da4dd4
Sina Atalay <79940989+sinaatalay@users.noreply.github.com>
2025-11-21 13:14:33 +0300

    Rename old tests

--- tests/schema/test_pydantic_error_handling.py
+++ tests/schema/test_pydantic_error_handling.py
@@ -1,5 +1,3 @@
-import pathlib
-
 import pydantic
 
 from rendercv.schema.pydantic_error_handling import parse_validation_errors
@@ -9,18 +7,11 @@ from rendercv.schema.rendercv_model_builder import (
 from rendercv.schema.yaml_reader import read_yaml
 
 
-def test_parse_validation_errors():
-    wrong_input_file_path = (
-        pathlib.Path(__file__).parent / "testdata" / "test_reader" / "wrong_input.yaml"
-    )
+def test_parse_validation_errors(testdata_dir):
+    wrong_input_file_path = testdata_dir / "wrong_input.yaml"
     wrong_input_dictionary = read_yaml(wrong_input_file_path)
 
-    expected_errors_file_path = (
-        pathlib.Path(__file__).parent
-        / "testdata"
-        / "test_reader"
-        / "expected_errors.yaml"
-    )
+    expected_errors_file_path = testdata_dir / "expected_errors.yaml"
     expected_errors = read_yaml(expected_errors_file_path)["expected_errors"]
 
     try:
