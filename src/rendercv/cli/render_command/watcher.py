484b101734dcc1e1523e66190aeac003ae62e4ed
Sina Atalay <79940989+sinaatalay@users.noreply.github.com>
2025-12-10 18:40:38 +0300

    Fix spelling

--- src/rendercv/cli/render_command/watcher.py
+++ src/rendercv/cli/render_command/watcher.py
@@ -36,7 +36,7 @@ def run_function_if_file_changes(file_path: pathlib.Path, function: Callable):
                     self.function()
                 except Exception as e:
                     # This means an unhandled error occurred in the function.
-                    # Don't surpress it
+                    # Don't suppress it
                     raise e
 
     event_handler = EventHandler(function)
